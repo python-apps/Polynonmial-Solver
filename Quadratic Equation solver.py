08416d48866db95f10633660fbfec223a8c4fbff
Arti0cle <78649410+ThirumuruganRA@users.noreply.github.com>
2021-02-11 19:14:39 +0530

    Add files via upload

---
+++ Quadratic Equation solver.py
@@ -0,0 +1,15 @@
+import cmath as m
+a = int(input("Enter the coefficient of x^2 "))
+b = int(input("Enter the coefficient x "))
+c = int(input("Enter the constant "))
+d = ((b**2) - 4*a*c)
+if d > 0:
+    print("The roots real and distinct")
+if d == 0:
+    print("The roots are real and equal")
+if d < 0:
+    print("There are no real roots for the given equation")
+e = m.sqrt(d)
+root1 = ((-b + e)/(2*a)) 
+root2 = ((-b - e)/(2*a))
+print("The Roots of the given equation are,",root1,"and",root2)
