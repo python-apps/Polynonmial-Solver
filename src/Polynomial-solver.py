614e66a6493420ecb001ee0d3268391885f0c0f1
Vishvanathan K <vishvanathank@hotmail.com>
2021-02-12 18:10:56 +0530

    Update Polynomial-solver.py
    Using numpy.polynomial for easy calculating

--- src/Polynomial-solver.py
+++ src/Polynomial-solver.py
@@ -1,15 +1,9 @@
 import cmath as m
-a = float(input("Enter the coefficient of x^2 :"))
-b = float(input("Enter the coefficient x :"))
-c = float(input("Enter the constant :"))
-d = ((b**2) - 4*a*c)
-if d > 0:
-    print("The roots real and distinct")
-if d == 0:
-    print("The roots are real and equal")
-if d < 0:
-    print("There are no real roots for the given equation")
-e = m.sqrt(d)
-root1 = ((-b + e)/(2*a)) 
-root2 = ((-b - e)/(2*a))
-print("The Roots of the given equation are,",root1,"and",root2)
+from numpy.polynomial import polynomial as P
+deg = input("Enter the degree of polynomial")
+if deg == "quad":
+        a = float(input("Enter the coefficient of x^2 :"))
+        b = float(input("Enter the coefficient x :"))
+        c = float(input("Enter the constant :"))        
+        fx = P([c,b,a])
+        print("The Roots of the given equation are,"P.root())
