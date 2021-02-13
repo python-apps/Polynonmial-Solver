da9a3694142eb504935e1354aee967075983b29a
Vishvanathan K <vishvanathank@hotmail.com>
2021-02-13 14:01:46 +0530

    Delete quadpoly.py

--- src/quadpoly.py
+++
@@ -1,8 +0,0 @@
-from numpy.polynomial import Polynomial as P
-def quadpoly():
-	  a = float(input("Enter the coefficient of x^2 :"))
-	  b = float(input("Enter the coefficient x :"))
-	  c = float(input("Enter the constant :"))        
-	  poly = P([c,b,a])
-	  print("The Roots of the given equation are," + poly.root())
-	
