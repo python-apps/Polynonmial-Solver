d897267570608d59d911c994f44a871c39f70311
Vishvanathan K <vishvanathank@hotmail.com>
2021-02-13 14:02:06 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,12 @@
-import quadpoly 
+from numpy.polynomial import Polynomial as P
+
+def quadpoly():
+	  a = float(input("Enter the coefficient of x^2 :"))
+	  b = float(input("Enter the coefficient x :"))
+	  c = float(input("Enter the constant :"))        
+	  poly = P([c,b,a])
+	  print("The Roots of the given equation are," + poly.root())
+                  
 deg = input("Enter the degree of polynomial")
 if deg == "quad":
          quadpoly()
