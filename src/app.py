29afbb42ca07374cd7d9baf92c6730228d764445
Vishvanathan K <vishvanathank@hotmail.com>
2021-02-15 09:26:20 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,27 +1,28 @@
+import cmath as m
+
 def quadpoly(a,b,c):
-	  d = ((b**2) - 4*a*c)
-	  e = m.sqrt(d)
-	  alpha = ((-b + e)/(2*a)) 
-	  beta = ((-b - e)/(2*a))
-	  return alpha,beta
-def cubpoly(a,b,c,d):
-	  alpha = (-b / a) / 3
-	  a1 = a 
-	  b1 = b + alpha*a
-	  c1 = c + alpha*b
-          beta , gamma = quadpoly(a1,b1,b2)
-	  return alpha , beta , gamma
-deg = input("Enter the degree of polynomial")
+    d = ((b**2) - 4*a*c)
+    e = m.sqrt(d)
+    alpha = ((-b + e)/(2*a)) 
+    beta = ((-b - e)/(2*a))
+    return str(alpha) , str(beta)
+def cubpoly(a,b,c):
+    alpha = -b / 3*a
+    b1 = b + alpha*a
+    c1 = c + alpha*b
+    beta , gamma = quadpoly(a,b1,c1)
+    return str(alpha) , beta , gamma
+deg = input("Enter the degree of polynomial:")
 if deg == "quad":
-	  a = float(input("Enter the coefficient of x^2 :"))
-	  b = float(input("Enter the coefficient x :"))
-	  c = float(input("Enter the constant :")) 
-          alpha , beta = quadpoly(a,b,c)
-	  print("The Roots of the given equation are,"+ alpha + " " + beta )
-elif deg == "cube":
-	  a = float(input("Enter the coefficient of x^3 :"))
-	  b = float(input("Enter the coefficient x^2 :"))
-	  c = float(input("Enter the coefficient x :"))
-	  d = float(input("Enter the constant :")) 
-	  alpha , beta , gamma = cubpoly(a,b,c,d)
-	  print("The Roots of the given equation are,"+ alpha + " " + beta + " " + gamma )
+    a = float(input("Enter the coefficient x^2 :"))
+    b = float(input("Enter the coefficient x :"))
+    c = float(input("Enter the constant :"))
+    alp , be = quadpoly(a,b,c)
+    print("The Roots of the given equation are,"+ alp + " " + be)
+elif deg == "cubic":
+    a3 = float(input("Enter the coefficient of x^3 :"))
+    b3 = float(input("Enter the coefficient x^2 :"))
+    c3 = float(input("Enter the coefficient x :"))
+    d3 = float(input("Enter the constant :"))
+    alpha , beta , gamma = cubpoly(a3,b3,c3,d3)
+    print("The Roots of the given equation are,"+ alpha + " " + beta + " " + gamma )
